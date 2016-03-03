242ec171b27d93ea1403ac437bb3d14872154120
Ha Huy <tongnguyen.hahuy@gmail.com>
2016-03-04 01:58:51 +0700

    dfsd

--- db/migrate/20160303021003_change_column_name_to_spree_users.rb
+++ db/migrate/20160303021003_change_column_name_to_spree_users.rb
@@ -1,6 +1,6 @@
 class ChangeColumnNameToSpreeUsers < ActiveRecord::Migration
   def change
-  	# rename_column :spree_users, :email_change, :change_email
+  	 rename_column :spree_users, :email_change, :change_email
 
   end
 end
